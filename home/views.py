6dcb4c931037bd3e8af7fd2550fb864ace77c976
BUDIREDDY ATCHUTH KUMAR <127076577+BUDIREDDYATCHUTHKUMAR@users.noreply.github.com>
2025-12-09 13:21:15 +0530

    58535

--- home/views.py
+++ home/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.generic import ListAPIView
+from products.models import Item
+from products.serializers import ItemSerializer
 
 # Create your views here.
+class MenuItemSerializer(ListAPIView):
+    queryset=Item.objects.all()
+    serializer_class=ItemSerializer
