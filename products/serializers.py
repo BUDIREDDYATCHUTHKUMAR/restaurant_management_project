afe1a87c69a4bd562e33b281cb5bfa8f62b1e911
BUDIREDDY ATCHUTH KUMAR <127076577+BUDIREDDYATCHUTHKUMAR@users.noreply.github.com>
2025-12-09 13:17:37 +0530

    58535

--- products/serializers.py
+++ products/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from .models import Item
-
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Item
-        fields = '__all__'
+        model=Item
+        fields=['id','item_name','item_price']
