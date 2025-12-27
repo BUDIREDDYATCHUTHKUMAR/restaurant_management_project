0efd9c992b62b4300c30ffc4fddbc150a3d52c0a
BUDIREDDY ATCHUTH KUMAR <127076577+BUDIREDDYATCHUTHKUMAR@users.noreply.github.com>
2025-12-27 10:34:59 +0530

    64759

--- orders/utils.py
+++ orders/utils.py
@@ -3,5 +3,5 @@ from .models import Coupon
 def generate_coupon_code(length=10):
     while True:
         code=get_random_string(length=length)
-        if not Coupon.objects.filter(code==code).exists():
+        if not Coupon.objects.filter(code=code).exists():
             return code
