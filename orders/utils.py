a74ef295cd11dc714a4d40bb515d3a0a18d017f5
BUDIREDDY ATCHUTH KUMAR <127076577+BUDIREDDYATCHUTHKUMAR@users.noreply.github.com>
2025-12-27 10:37:00 +0530

    64759

--- orders/utils.py
+++ orders/utils.py
@@ -2,6 +2,6 @@ from django.utils.crypto import get_random_string
 from .models import Coupon
 def generate_coupon_code(length=10):
     while True:
-        code=get_random_string(length=length)
+        code=get_random_string(length=length).upper()
         if not Coupon.objects.filter(code=code).exists():
             return code
