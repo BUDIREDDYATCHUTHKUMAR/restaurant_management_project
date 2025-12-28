6a059db09301a491be7e370994dbe38c1a9386d1
BUDIREDDY ATCHUTH KUMAR <127076577+BUDIREDDYATCHUTHKUMAR@users.noreply.github.com>
2025-12-28 12:15:04 +0530

    64757

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Order,OrderItem,OrderStatus,Coupon
+
+@admin.register(OrderStatus)
+class OrderStatusAdmin(admin.ModelAdmin):
+    list_display=['name']
+
+@admin.register(coupon)
+class CouponAdmin(admin.ModelAdmin):
+    list_display=['code','discount_percentage','is_active','valid_from','valid_to']
+    list_filter=['is_active']
+class OrderItemInline(admin.TabularInline):
+    model=OrderItem
+    extra=1
+@admin.register(order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display=['id','customer_name','total_price','status','cretaed_at']
+    list_filter=['status','created_at']
+    inlines=[OrderItemInline]
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display=['order','item','quantity','price']
 
 # Register your models here.
