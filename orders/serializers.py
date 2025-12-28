a7c24977194001d08c94c68901eca547449c073b
BUDIREDDY ATCHUTH KUMAR <127076577+BUDIREDDYATCHUTHKUMAR@users.noreply.github.com>
2025-12-28 11:59:38 +0530

    64757

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,1 +1,24 @@
-from rest_framework
+from rest_framework import serializers
+from .models import Order,OrderItem,OrderStatus,Coupon
+from products.serializers import ItemSerializer
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    item=ItemSerializer(read_only=True)
+    class Meta:
+        model=OrderItem
+        fields=['item','quantity','price']
+class OrderHistorySerializer(serializer.ModelSerializer):
+    items=OrderItemserializer(source='order_items',many=True,),read_only=True)
+    class Meta:
+        model=Order
+        fields=['id','created_at','total_price','status','items']
+class OrderSerializer(serializer.ModelSerializer):
+    class Meta:
+        model=Order
+        fields='__all__'
+
+
+class CouponSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Coupon
+        fields='__all__'
