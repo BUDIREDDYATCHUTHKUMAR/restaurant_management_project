c948de69d0c00e87e467a496b40b2829b1786aa5
BUDIREDDY ATCHUTH KUMAR <127076577+BUDIREDDYATCHUTHKUMAR@users.noreply.github.com>
2025-12-28 12:18:52 +0530

    64757

--- orders/urls.py
+++ orders/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import CouponValidationView
 
 urlpatterns = [
-    path("coupons/validate/", CouponValidationView.as_view(), name="coupons_validate"),
+    path("coupons/validate/", CouponValidationView.as_view(), name="couons_validate"),
+    path("my-orders/",UserOrderHistoryView.as_view(),name='user-order-history'),
+    
 ]
